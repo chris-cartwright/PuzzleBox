0acba7e852df93690d59a5208d127812a9bece9a
chris-cartwright <cartwright.christopher@gmail.com>
2014-03-20 22:45:23 -0400

    Initial setup and code logic

---
+++ PuzzleBox.ino
@@ -0,0 +1,30 @@
+
+#include <LiquidCrystal.h>
+#include <TinyGPS++.h>
+#include <Servo.h>
+
+void setup()
+{
+  // Set up pins
+  // Set up LCD
+  // Display 'Welcome'
+  // Display 'Searching for signal...'
+}
+
+void loop()
+{
+  // Check to see if unlock circuit is hooked up
+  // Check to see if we have aquired a GPS signal
+    // Calculate distance from here to target
+    // If distance is within range
+      // Enable servo
+      // Move to unlock position
+      // disable servo
+      // Display congradulations
+    // else
+      // Display distance
+    // end
+    // Pause for x seconds
+    // Turn off
+  // else flash dots
+}
